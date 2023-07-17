c27c8b52c90bac20dd82e35cf8b91e2014d89d57
alerciosilva9 <silvaalercio@gmail.com>
2023-07-16 23:50:28 -0300

    corrigindo tabela

--- src/routes/Transferencias.jsx
+++ src/routes/Transferencias.jsx
@@ -4,31 +4,37 @@ import Formulario from "../components/Formulario";
 
 function Transferencias() {
 
-
+  const [total, setTotal] = useState('');
+  const [first,setFisrt] = useState(true)
   const conta_id = useLocation().state.some
   const[transferencias,setTransferencias] = useState([])
 
+
   const getTransferencias = async(params) => {
-    
+    let sum = 0
     try {
-      const url = new URL('http://localhost:8080/transferencias/'+conta_id);
-      
+      const url = new URL('http://localhost:8080/transferencias/'+conta_id); 
       Object.keys(params).forEach(key => params[key]!=''?url.searchParams.append(key, params[key]):console.log(" "));
-     
-      
-      console.log(url)
+           
       fetch(url)
         .then(response => response.json())
         .then(data => {
           setTransferencias(data);
         });
+      
     } catch (error) {
         console.log(error)
     }
+    let lista = transferencias
+    lista.forEach(t => {
+      sum += t.valor;
+    });
+    setTotal(sum)
+
   }
   useEffect(()=> {
         getTransferencias()
-  },[])
+  })
     
 
     return (
@@ -38,6 +44,7 @@ function Transferencias() {
         <h3>FIltros de Busca</h3>
         <Formulario onSubmit={getTransferencias} />
           <h1>Transferências</h1>
+          <p>Saldo Total = {total}</p>
           <table  class="table">
           <thead class="thead-dark">
             <tr>
@@ -48,7 +55,8 @@ function Transferencias() {
             </tr>
             </thead>
             <tbody>
-            {transferencias.map(transferencias => (
+
+            {transferencias.length!==0?transferencias.map(transferencias => (
               <tr key={transferencias.id}>
                 <td>{transferencias.dataTransferencia}</td>
                 <td>{transferencias.valor}</td>
@@ -56,7 +64,8 @@ function Transferencias() {
                 <td>{transferencias.operador}</td>
                 
               </tr>
-            ))}</tbody>
+            )):<p>Sem registros de transferencias</p>}
+            </tbody>
           </table>
         </>
       
